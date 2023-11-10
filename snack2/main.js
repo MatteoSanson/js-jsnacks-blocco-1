123f25d09f08ad9a6778c6f074fa7f2eea95635b
MatteoSanson <matteo.sanson@gmail.com>
2023-11-10 11:25:37 +0100

    fix: snack2

--- snack2/main.js
+++ snack2/main.js
@@ -6,10 +6,12 @@ console.log (parolaPrima);
 const parolaSeconda = prompt ('inserisci la seconda parla');
 console.log(parolaSeconda);
 
-if (parolaPrima.length > parolaSeconda.length) {
-    console.log('La parola più lunga è:', parolaPrima);
-  } else if (parolaPrima.length < parolaSeconda.length) {
+if (parolaPrima.length < parolaSeconda.length) {
+    console.log('La parola più corta è:', parolaPrima);
     console.log('La parola più lunga è:', parolaSeconda);
-  } else {
+} else if (parolaPrima.length > parolaSeconda.length) {
+    console.log('La parola più corta è:', parolaSeconda);
+    console.log('La parola più lunga è:', parolaPrima);
+} else {
     console.log('Le parole hanno la stessa lunghezza.');
-  }
+}
