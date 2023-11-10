6aaf0d3443496e0bab627c374770d6e6039dd68d
MatteoSanson <matteo.sanson@gmail.com>
2023-11-10 12:03:46 +0100

    feat: snack3

---
+++ snack3/main.js
@@ -0,0 +1,17 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+'use strict';
+
+let somma = 0;
+
+for (let i = 0; i < 10; i++) {
+  let numeroUtente = parseInt(prompt("Inserisci un numero:"));
+  if (!isNaN(numeroUtente)) {
+    somma += numeroUtente;
+    console.log (numeroUtente);
+  } else {
+    alert("Non hai inserito un numero.");
+  }
+}
+
+console.log("La somma dei numeri che hai inserito è: " + somma);
