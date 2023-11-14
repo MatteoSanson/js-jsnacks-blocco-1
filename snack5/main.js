3f6d4ecf392c77db2e92db56e6a7530ee674516f
MatteoSanson <matteo.sanson@gmail.com>
2023-11-14 13:37:27 +0100

    feat: snack5

--- snack5/main.js
+++ snack5/main.js
@@ -1,6 +1,6 @@
 // Fai inserire un numero, che chiameremo N, all’utente.
 // Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo.
-'use strict'
+'use strict';
 
 console.log ('ciao');
 
